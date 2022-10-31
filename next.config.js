b8072ddf1e145f731de1a714b35695a97f9725c7
HARVEYNASH\linhhoangtm1 <linhhoangtm1@nashtechglobal.com>
2022-10-31 10:00:42 +0700

    remove folder

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-
-
 module.exports = {
   webpack: (config) => {
     config.module.rules.push({
@@ -20,6 +7,4 @@ module.exports = {
     config.resolve.fallback = { module: false };
     return config;
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
 }
