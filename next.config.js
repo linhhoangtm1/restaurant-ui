0e95fd7c2407cc5b97574588d23af5352d3c2a34
HARVEYNASH\linhhoangtm1 <linhhoangtm1@nashtechglobal.com>
2022-10-31 09:34:10 +0700

    config yml

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
+
 module.exports = {
   webpack: (config) => {
     config.module.rules.push({
@@ -7,4 +20,6 @@ module.exports = {
     config.resolve.fallback = { module: false };
     return config;
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
